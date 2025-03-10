76885b97cb280b237f09881e8d1abece9334b429
Hcha-byte <harveychaney@gmail.com>
2025-03-10 14:24:36 +1300

    css and add_post

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from app import db
 from app.models import Post
 
 main = Blueprint('main', __name__)
@@ -16,3 +18,15 @@ def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
+@main.route('/add', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post = Post(title=title, content=content)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been published!', 'success')
+        return redirect(url_for('main.home'))
+	
+    return render_template('add_post.html', title='Add Post')
