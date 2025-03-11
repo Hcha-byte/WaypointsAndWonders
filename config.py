5ae264bcf3da1f175fdb94024406e3343cb1cc5b
Hcha-byte <harveychaney@gmail.com>
2025-03-11 15:35:50 +1300

    db fixes

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you_will_never_guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:AWFPEJtYfLYfZsgbCcErKnORszcgCNRR@caboose.proxy.rlwy.net:12279/railway')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:mpBThkxtbtVJnhvfRGIrxJNhdLmVVRUS@switchyard.proxy.rlwy.net:23186/railway' if os.environ.get('IS_DEV') is "TRUE" else "postgresql://postgres:AWFPEJtYfLYfZsgbCcErKnORszcgCNRR@caboose.proxy.rlwy.net:12279/railway")
