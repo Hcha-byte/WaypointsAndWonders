a09b1abba92e470d6772afaf5e62db3035163108
Hcha-byte <harveychaney@gmail.com>
2025-06-19 12:28:50 -0600

    foo33

---
+++ app/search/config.py
@@ -0,0 +1,5 @@
+from requests.auth import HTTPBasicAuth
+
+TOSHI_URL = "http://toshi-deploy-main.up.railway.app"
+AUTH = HTTPBasicAuth("user", "kFBIj6YqkZzSS6F8yDyzhO+WTOmyt3t9")
+INDEX_NAME = "posts"
